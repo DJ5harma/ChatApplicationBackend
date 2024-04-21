3530316ac1f5107eb782ef99c86ae3303352f567
DJ5harma <sunubhargav2004@gmail.com>
2024-04-21 14:05:56 +0530

    forgot to delete getUsers controller

--- controllers/getData.js
+++ controllers/getData.js
@@ -3,14 +3,11 @@ import User from "../models/user.model.js";
 
 export default async function getData(req, res) {
 	const { userId } = req;
-
 	try {
 		const messages = await Message.find({
 			$or: [{ senderId: userId }, { receiverId: userId }],
 		});
 		const users = await User.find().select(["-password", "-updatedAt"]); // Getting all the users from mongoDB
-
-		console.log(users);
 
 		return res.json({
 			messages,
